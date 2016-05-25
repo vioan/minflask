8bb1026b7a3a8f9f3a08ee81bf13a836932a4046
Ioan Vancea <ionut@vioan.ro>
2016-05-25 14:04:14 +0200

    Initial import of a minimal Flask app

---
+++ app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello from FLASK"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
