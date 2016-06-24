2b48e786c3e2439876ff41fc82bd814b42d40ca4
Ioan Vancea <ionut@Ioans-MacBook-Air.local>
2016-06-24 11:37:36 +0200

    Revert "Added the output for hostname"
    This reverts commit 490fa2d10d8a61ede28b794ed446e569f2f30318.

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask
-import socket
+#!/usr/bin/env python
 
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello from FLASK. My Hostname is: %s \n" % (socket.gethostname())
+    return "Hello from FLASK"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
