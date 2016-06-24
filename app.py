635d3d47ad118f2ab85a264d8935eff32b4f8f9a
Ioan Vancea <ionut@vioan.ro>
2016-06-24 11:41:06 +0200

    Added output for hostname

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python
-
 from flask import Flask
+import socket
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello from FLASK"
+    return "Hello from FLASK. My Hostname is: %s \n" % (socket.gethostname())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
